29069032d5236c19aefa7e5e5e1fa9c354659766
Tgreenh <tom_greenhalgh@hotmail.com>
2022-11-12 00:11:55 +0200

    Display message if device orientation is not supported

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import './style.css';
 
 const messageElement = document.createElement("div");
-const messageTextNode = document.createTextNode("No orientation data");
+const messageTextNode = document.createTextNode("");
 messageElement.className = "message";
 messageElement.appendChild(messageTextNode);
 
 document.body.appendChild(messageElement);
+
+if (window.DeviceOrientationEvent) {
+  messageTextNode.nodeValue = "Device orientation supported - no data yet";
+}
+else {
+  messageTextNode.nodeValue = "Device orientation not supported";
+}
 
 window.addEventListener('deviceorientation', (event) => {
   const message = `Alpha: ${event.alpha} Beta: ${event.beta} Gamma: ${event.gamma}`;
